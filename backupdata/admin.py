bd5d22d61f1cfb42302fd83223651bad2016987e
Issac Kelly <issac@issackelly.com>
2016-05-10 20:17:24 -0700

    Update messages admin.

--- backupdata/admin.py
+++ backupdata/admin.py
@@ -1,7 +1,21 @@
 from backupdata.models import Channel, Message, ChannelMember
 from django.contrib import admin
 
+class MessageAdmin(admin.ModelAdmin):
+    list_display = [
+        'channel',
+        'user',
+        'text'
+    ]
+    ordering = [
+        '-ts'
+    ]
+    list_filter = [
+        'channel',
+        'user'
+    ]
+
 # Register your models here.
 admin.site.register(Channel)
-admin.site.register(Message)
+admin.site.register(Message, MessageAdmin)
 admin.site.register(ChannelMember)
